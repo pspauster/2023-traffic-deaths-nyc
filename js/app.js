6da0eb576ecdec093c8c531b18d1863bd7f48f7a
Patrick Spauster <patrick.spauster@gmail.com>
2023-05-21 18:33:07 -0400

    edits based on CW feedback

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ $.ajax({
 
     // Attach click event handlers to the buttons
     $('#pedestrians').click(function () {
+        $.when()
         map.setFilter('circle-my-points', ['!=', ['get', 'number_of_pedestrians_killed'], "0"]);
     });
 
@@ -29,13 +30,15 @@ $.ajax({
     });
 
     $('#all').click(function () {
-        map.setFilter('circle-my-points', null);
+        map.setFilter('circle-my-points', ['!=', ['get', 'number_of_persons_killed'], "0"]);
     });
 
     // Add class to selected features
     $('.button').on('click', function () {
-        $('.button').removeClass("selected")
-        $(this).addClass("selected")
+        $('.button').removeClass("selected");
+        $(this).addClass("selected");
+        // when button clicked, close popups
+        $('.mapboxgl-popup').remove();
     })
 
     //add a running total to the main text
@@ -45,28 +48,36 @@ $.ajax({
         return sum + (isNaN(value) ? 0 : value);
     }, 0);
 
-    document.getElementById('count').innerHTML = count;
-
     var count_ped = data.features.reduce(function (sum, feature) {
         var value = parseFloat(feature.properties.number_of_pedestrians_killed);
         return sum + (isNaN(value) ? 0 : value);
     }, 0);
-
-    document.getElementById('count_ped').innerHTML = count_ped;
 
     var count_bike = data.features.reduce(function (sum, feature) {
         var value = parseFloat(feature.properties.number_of_cyclist_killed);
         return sum + (isNaN(value) ? 0 : value);
     }, 0);
 
-    document.getElementById('count_bike').innerHTML = count_bike;
-
     var count_motor = data.features.reduce(function (sum, feature) {
         var value = parseFloat(feature.properties.number_of_motorist_killed);
         return sum + (isNaN(value) ? 0 : value);
     }, 0);
 
-    document.getElementById('count_motor').innerHTML = count_motor;
+    //append all names to dom
+    var classesAndVariables = [
+        { className: 'count', variable: count },
+        { className: 'count_ped', variable: count_ped },
+        { className: 'count_bike', variable: count_bike },
+        { className: 'count_motor', variable: count_motor }
+    ];
+
+    classesAndVariables.forEach(function (item) {
+        var spans = document.getElementsByClassName(item.className);
+
+        Array.from(spans).forEach(function (span) {
+            span.textContent += item.variable;
+        });
+    });
 
     //set up map
     const map = new mapboxgl.Map({
@@ -78,8 +89,6 @@ $.ajax({
         bearing: 0,
         pitch: 0
     });
-
-
 
     //add sources and layers
     map.on('load', function () {
@@ -107,12 +116,16 @@ $.ajax({
                         'red',
                         'Driver Inattention/Distraction',
                         'purple',
-                        'Driver Inexperience',
-                        'green',
                         'Traffic Control Disregarded',
+                        'green',
+                        'Alcohol Involvement',
                         'gold',
-                        'Oversized Vehicle',
+                        'Pedestrian/Bicyclist/Other Pedestrian Error/Confusion',
                         'blue',
+                        'Failure to Yield Right-of-Way',
+                        'pink',
+                        'Unspecified',
+                        'gray',
                     /*other*/ '#000000']
                 ],
                 'circle-color': [
@@ -122,12 +135,16 @@ $.ajax({
                     'red',
                     'Driver Inattention/Distraction',
                     'purple',
-                    'Driver Inexperience',
-                    'green',
                     'Traffic Control Disregarded',
+                    'green',
+                    'Alcohol Involvement',
                     'gold',
-                    'Oversized Vehicle',
+                    'Pedestrian/Bicyclist/Other Pedestrian Error/Confusion',
                     'blue',
+                    'Failure to Yield Right-of-Way',
+                    'pink',
+                    'Unspecified',
+                    'gray',
                     /*other*/ '#000000'
                 ]
             },
@@ -136,10 +153,12 @@ $.ajax({
         const layers = [
             'Unsafe Speed',
             'Driver Inattention/Distraction',
-            'Driver Inexperience',
             'Traffic Control Disregarded',
-            'Oversized Vehicle',
-            'Other/Unspecified',
+            'Alcohol Involvement',
+            'Pedestrian/Bicyclist/Other Pedestrian Error/Confusion',
+            'Failure to Yield Right-of-Way',
+            'Unspecified',
+            'Other'
         ];
         const colors = [
             'red',
@@ -147,6 +166,8 @@ $.ajax({
             'green',
             'gold',
             'blue',
+            'pink',
+            'gray',
             'black'
         ];
 
@@ -169,8 +190,9 @@ $.ajax({
         });
 
 
-    })
 
+
+    })
     //create popups and conditional sentence for each point
     map.on('click', 'circle-my-points', (e) => {
 
@@ -184,11 +206,12 @@ $.ajax({
 
 
         //popups
-        new mapboxgl.Popup()
+        const popup = new mapboxgl.Popup()
             .setLngLat(e.lngLat)
             .setHTML(`${e.features[0].properties.number_of_persons_killed} ${singPlural} ${verb} killed by a ${e.features[0].properties.vehicle_type_code1.toLowerCase()} due to ${e.features[0].properties.contributing_factor_vehicle_1.toLowerCase()} at ${moment(e.features[0].properties.crash_time, "hh:mm").format("h:mm a")} on ${moment(e.features[0].properties.crash_date).format("dddd, MMMM Do")}`)
             .addTo(map)
     });
+
 
 
     let hoveredStateId = null
@@ -222,6 +245,107 @@ $.ajax({
         }
         hoveredStateId = null;
     });
+
+
+    //Make D3 chart!
+
+    // Select the existing container with the id "chart"
+    const chartContainer = d3.select('#chart');
+
+    // Define the dimensions and margins of the chart
+    const margin = { top: 30, right: 20, bottom: 30, left: 136 };
+    const containerWidth = chartContainer.node().getBoundingClientRect().width;
+    const width = containerWidth - margin.left - margin.right;
+    const height = 200 - margin.top - margin.bottom;
+
+    // Create an SVG element for the chart inside the existing container
+    const svg = chartContainer
+        .append('svg')
+        .attr('width', containerWidth)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    // Count the occurrences of each category
+    const counts = d3.rollups(
+        data.features,
+        v => v.length,
+        d => d.properties.contributing_factor_vehicle_1
+    );
+
+    // Combine categories with less than one observation into an "Other" category
+    const minimumObservations = 2;
+    let combinedCounts = counts.filter(d => d[1] >= minimumObservations);
+    const otherCategories = counts.filter(d => d[1] < minimumObservations);
+    if (otherCategories.length > 0) {
+        const otherCount = otherCategories.reduce((acc, curr) => acc + curr[1], 0);
+        combinedCounts.push(['Other', otherCount]);
+    }
+
+    combinedCounts.sort((a, b) => b[1] - a[1]);
+
+    const colorScale = d3
+        .scaleOrdinal()
+        .domain([
+            'Unsafe Speed',
+            'Driver Inattention/Distraction',
+            'Traffic Control Disregarded',
+            'Alcohol Involvement',
+            'Pedestrian/Bicyclist/Other Pedestrian Error/Confusion',
+            'Failure to Yield Right-of-Way',
+            'Unspecified',
+            'Other',
+        ])
+        .range(['red', 'purple', 'green', 'gold', 'blue', 'pink', 'gray', '#000000']);
+
+
+    // Set up the y and x scales
+    const y = d3.scaleBand()
+        .domain(combinedCounts.map(d => d[0]))
+        .range([0, height])
+        .padding(0.1);
+
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(combinedCounts, d => d[1])])
+        .range([0, width]);
+
+    // Create the bars of the bar chart
+    svg.selectAll('.bar')
+        .data(combinedCounts)
+        .enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', 0)
+        .attr('width', d => x(d[1]))
+        .attr('y', d => y(d[0]))
+        .attr('height', y.bandwidth())
+        .style('fill', d => colorScale(d[0]));
+        ;
+
+    // Add y-axis to the chart
+    svg.append('g')
+        .call(d3.axisLeft(y))
+        .call(d3.axisLeft(y).tickSize(0))
+        .selectAll('.tick text')
+        .text(d => {
+          if (d === 'Pedestrian/Bicyclist/Other Pedestrian Error/Confusion') {
+            return 'Ped/Bike Error/Confusion';
+          }
+          return d;
+        });
+
+    // Add x-axis to the chart
+    svg.append('g')
+        .attr('transform', `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    // Add a title to the chart
+    svg.append('text')
+        .attr('x', width / 2)
+        .attr('y', 0 - (margin.top / 2))
+        .attr('text-anchor', 'middle')
+        .text('Total accidents by cause');
+
 
 });
 
